2aa842aec8e5f00c73b53d3c4ab94f1a4b39f9b3
Keo Tan <khantminhtut009@gmail.com>
2025-03-11 22:05:22 +0630

    Add files via upload

---
+++ 3. Where Clause, Like statement, %.sql
@@ -0,0 +1,23 @@
+-- WHERE Clause
+
+SELECT *
+FROM employee_salary
+WHERE salary <= 50000;
+
+
+
+
+-- AND OR NOT -- Logical Operators
+
+
+
+-- LIKE Statement
+-- % and _
+SELECT *
+FROM employee_demographics
+WHERE birth_date LIKE "1989%"
+
+
+
+
+
